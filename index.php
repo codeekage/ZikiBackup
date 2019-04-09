7117b6d0d3a63ea2ccd63e15d0db1f041c2a6abe
Maybelle-Mayflower <maybellekakulu@gmail.com>
2019-04-10 00:44:58 +0100

    new commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include('public/header.php');?>
+<!--/end includes-->
+            <div class="row mx-0 d-flex flex-column">
+                <h5 class="landing-page-greeting">Hello Mark Essien,</h5>
+                <div class="d-flex flex-row">
+                    <h1 class="landing-page-main-header">Welcome to<br/>The Platform </h1>
+                    <a href="/auth.php" class="landing-page-btn align-self-end">Join Now</a>
+                </div>
+                <p class="col-md-4 mr-auto landing-page-info">Welcome to the Utopian future, you no longer need to sign up to enjoy your favourite news sources, Just Join us now to have access and your details will be automatically generated. </p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ temp_landing_page.php
@@ -0,0 +1,54 @@
+<?php
+require_once 'Authentication/auth/googleinit.php';
+
+print_r($_SESSION);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Temp</title>
+
+        <style type="text/css">
+
+        #information-container {
+            width: 400px;
+            margin: 50px auto;
+            padding: 20px;
+            border: 1px solid #cccccc;
+        }
+
+        .information {
+            margin: 0 0 30px 0;
+        }
+
+        .information label {
+            display: inline-block;
+            vertical-align: middle;
+            width: 150px;
+            font-weight: 700;
+        }
+
+        .information span {
+            display: inline-block;
+            vertical-align: middle;
+        }
+
+        .information img {
+            display: inline-block;
+            vertical-align: middle;
+            width: 100px;
+        }
+
+        </style>
+    </head>
+
+    <body>
+        <h1>Temporary Landing Page (After login)</h1>
+        
+
+    </body>
+</html>
